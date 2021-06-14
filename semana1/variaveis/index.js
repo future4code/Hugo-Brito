e0c55930cce9afe959877b202f7b5e47a61cd696
Hugo Brito <hugo_oliveira34@hotmail.com>
2021-06-14 16:12:03 -0300

    Variaveis JavaScript

---
+++ semana1/variaveis/index.js
@@ -0,0 +1,129 @@
+// Interpretção de códigos: 
+
+// --- Exercicio 1 ---
+
+// let a = 10
+// será definido o valor 10 para a
+
+// let b = 10
+// será definido o valor 10 para b
+
+// console.log(b)
+// será impresso o valor atual de b = 10
+
+// b = 5
+// será definido agora como b=5
+
+// console.log(a, b)
+// será impresso o valor de 10 para a
+// será impresso o valor de 5 para b 
+
+// --- Exercicio 2 ---
+
+// let a = 10
+// será definado o valor 10 para a
+
+// let b = 20
+// será definiado o valor 20 para b
+
+// let c
+// atribui-se a c um valor 
+
+// c = a
+// será definido o valor 10 para a
+
+// b = c
+// será definido o valor 10 para c
+
+// a = b
+// será definido o valor 10 para b
+
+// console.log(a, b, c)
+// será impresso o valor de 10 para a, b e c
+
+// --- Exercício 3 ---
+
+// let p = prompt("Quantas horas você trabalha por dia?")
+// "p" poderia ser atribuido como horasTrabalhadasPorDia
+
+// let t = prompt("Quanto você recebe por dia?")
+// "t" poderia ser atribuido como valorRecebidoPorDia
+
+//alert(`Voce recebe ${t/p} por hora`)
+// "${t/p}" poderia ser definido como ${horasTrabalhadasPorDia/valorRecebidoPorDia}
+
+
+// --- Exercícios de escrita de código ---
+
+// --- Exercícios 1 ---
+
+// const nome = prompt('Informe seu nome')
+
+// const idade = prompt('Informe sua idade') 
+
+// Apareceu uma mensagem de erro estranha
+
+// console.log('tipo de nome: ', typeof nome)
+
+// console.log('tipo de idade: ', typeof idade)
+
+// console.log('Olá', nome, idade)
+
+// novamente apareceu um erro de extensão, além de aparecer na tela um local para inserir o nome e a idade como havia sido programado para aparecer.
+
+// --- Exercício 2 ---
+
+// const pergunta1 = 'Você gosta de comer?'
+
+// const pergunta2 = 'Você gosta de música?'
+
+// const pergunta3 = 'Você gosta de dirigir?'
+
+// const resposta1 = prompt(pergunta1)
+
+// const resposta2 = prompt(pergunta2)
+
+// const resposta3 = prompt(pergunta3)
+
+// console.log(pergunta1, resposta1)
+
+// console.log(pergunta2, resposta2)
+
+// console.log(pergunta3, resposta3)
+
+// foram feitas perguntas e respostas que usuario pode responder no console e o dev conseguiu ver corretamente
+
+// --- Exercício 3 ---
+
+// let a = 10
+// let b = 25
+// let c = 
+
+// console.log(a, b)
+
+// c = a //10
+
+// a = b //25
+
+// b = c //10
+
+// console.log(a, b)
+
+// Pela sequencia de comando ao final temos a valendo 25 e b valendo 10
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
