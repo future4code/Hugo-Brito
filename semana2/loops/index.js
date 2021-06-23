1867b6ab8d22654d59fad1ed3070581972663722
Hugo Brito <hugo_oliveira34@hotmail.com>
2021-06-23 20:03:00 -0300

    Atividade Loops 80%

---
+++ semana2/loops/index.js
@@ -0,0 +1,148 @@
+// --- Exercícios de interpretação de código ---
+
+// # 1
+
+// let valor = 0
+// for(let i = 0; i < 5; i++) {
+//   valor += i
+// }
+// console.log(valor)
+
+// R: ??????????????
+
+// # 2
+
+// const lista = [10, 11, 12, 15, 18, 19, 21, 23, 25, 27, 30]
+// for (let numero of lista) {
+//   if (numero > 18) {
+// 		console.log(numero)
+// 	}
+// }
+
+// a) Será impresso no console os numeros maiores que 18 no console.
+
+// b) Não, teria que ser usado o indexOf() dentro do bloco for ???????
+
+// # 3
+
+// const quantidadeTotal = Number(prompt("Digite a quantidade de linhas: "))
+// let quantidadeAtual = 0
+// while(quantidadeAtual < quantidadeTotal){
+//   let linha = ""
+//   for(let asteriscos = 0; asteriscos < quantidadeAtual + 1; asteriscos++){
+//     linha += "*"
+//   }
+//   console.log(linha)
+//   quantidadeAtual++
+// }
+
+
+// R: ???????
+
+// --- Exercícios de escrita de código ---
+
+// # 1
+
+// const bichinhos = []
+
+// const bichosDeEstimação = Number(prompt('Quantos bichinhos de estimação você tem ?'))
+
+// const numeroDePets = (bichosDeEstimação)
+
+
+// if(numeroDePets === 0){
+     
+//     console.log('Que pena! Você pode adotar um pet!')
+
+// }else if(numeroDePets > 0) {
+
+//     let repeticao = 0
+//     while (repeticao < bichosDeEstimação) {
+
+//     let nomeDosPets = prompt('Diga o nome dos pets ?')
+        
+//     bichinhos.push(nomeDosPets)
+//     repeticao += 1
+
+//    }
+
+// }
+
+// console.log(bichinhos)
+
+
+// # 2
+
+// const arrayOriginal = []
+
+// const quantidade = Number(prompt('Insira numeros em uma array :'))
+
+// while (arrayOriginal.length < quantidade){
+
+//     let preenchaArray = Number(prompt('Insira os numeros para o array :'))
+
+//     arrayOriginal.push(preenchaArray)
+    
+// }
+
+// console.log(arrayOriginal)
+
+
+// // a)
+
+// for (i=0 ; i < arrayOriginal.length ; i++) {
+
+//     console.log(arrayOriginal[i])
+
+// }
+
+// // b)
+
+// for (i=0 ; i < arrayOriginal.length ; i++) {
+
+//     console.log(arrayOriginal[i] / 10)
+
+// }
+
+// c)
+
+// const arrayLetraC = []
+
+// for (i=0 ; i < arrayOriginal.++)length ; i {
+
+//     if(arrayOriginal[i] % 2 === 0){
+
+//         arrayLetraC.push(arrayOriginal[i])
+//     }
+
+// }
+
+// console.log(arrayLetraC)
+
+// d)
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
