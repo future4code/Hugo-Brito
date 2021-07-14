e7f3143da07d8fbed454ee091a336bbe151fb74a
Hugo Brito <hugo_oliveira34@hotmail.com>
2021-07-14 17:07:13 -0300

    insta4 80% concluido

---
+++ semana5/atividade-react3/insta4/src/App.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import styled from 'styled-components'
+import Post from './components/Post/Post';
+
+const MainContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  align-items: center;
+`
+
+class App extends React.Component {
+  render() {
+    return (
+      <MainContainer>
+
+        <Post
+          nomeUsuario={'Hugo'}
+          fotoUsuario={'https://picsum.photos/50/50'}
+          fotoPost={'https://picsum.photos/200/150?a=1'}
+        />
+
+        <Post
+          nomeUsuario={'Felipe'}
+          fotoUsuario={'https://picsum.photos/50/50'}
+          fotoPost={'https://picsum.photos/200/150?a=2'}
+        />
+
+        <Post
+          nomeUsuario={'Vini'}
+          fotoUsuario={'https://picsum.photos/50/50'}
+          fotoPost={'https://picsum.photos/200/?a=3'}
+        />
+
+      </MainContainer>
+    );
+  }
+}
+
+export default App;
